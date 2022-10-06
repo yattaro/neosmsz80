8bd23ebd58a1bcbbbb433c5d6fd6507453de3f27
Yattaro <julian@juliang.dev>
2022-10-06 06:27:10 -0500

    Began implementing VDP control logic

--- emu/sms_vdp.cpp
+++ emu/sms_vdp.cpp
@@ -25,5 +25,33 @@ BYTE sms_vdp::read_hcounter()
 
 void sms_vdp::write_control_word(BYTE data)
 {
-    (void)data;
+    if(is_second_control_write)
+    {
+        // Write MSB
+        control_word &= 0xFF;
+        control_word |= data << 8;
+        is_second_control_write = false;
+
+        switch(getControlWordCodeRegister())
+        {
+
+        }
+    }
+}
+
+/*
+ * Returns address stored in control register used to determine where port
+ * writes to 0xBE are stored
+ */
+WORD sms_vdp::getControlWordAddressRegister()
+{
+    return control_word & 0x3FFF;
+}
+
+/*
+ * Returns nibble used to determine how port writes to 0xBE are executed
+ */
+BYTE sms_vdp::getControlWordCodeRegister()
+{
+    return (BYTE)(control_word >> 14);
 }
--- emu/sms_vdp.h
+++ emu/sms_vdp.h
@@ -12,12 +12,22 @@ public:
     void write_control_word(BYTE data);
 
 private:
+    WORD getControlWordAddressRegister();
+    BYTE getControlWordCodeRegister();
     BYTE vram[0x4000];
     BYTE cram[0x20];
     BYTE vdp_regs[0xB];
     BYTE vcounter;
     BYTE hcounter;
     WORD control_word;
+    enum class ControlMode
+    {
+        write_vram_and_increment = 0,
+        write_vram = 1,
+        write_and_write_regs = 2,
+        write_cram = 3
+    };
+    BYTE read_buffer;
     bool is_second_control_write;
 };
 
